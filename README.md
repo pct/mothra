0e8e4a5ce68ae8ca4ce41d2cbdae4fe4c64e566f
Jin-Sih Lin <linpct@gmail.com>
2015-01-05 08:54:27 +0800

    restruct

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Mothra
 
-TODO: NOT done yet
+A FreeBSD send-pr command line tool for bugzilla system.
 
-## ref
-
-http://www.bugzilla.org/docs/4.4/en/html/api/Bugzilla/WebService/Server/JSONRPC.html
+[https://bugs.freebsd.org/bugzilla/](https://bugs.freebsd.org/bugzilla/)
 
 ## Installation
 
@@ -23,9 +21,38 @@ Or install it yourself as:
     $ gem install mothra
 
 ## Usage
+### search 
+ 
 
-TODO: Write usage instructions here
+## Reference
+1. [https://bugs.freebsd.org/bugzilla/](https://bugs.freebsd.org/bugzilla/)
+2. [http://www.bugzilla.org/docs/4.4/en/html/api/Bugzilla/WebService/Server/JSONRPC.html](http://www.bugzilla.org/docs/4.4/en/html/api/Bugzilla/WebService/Server/JSONRPC.html)
 
+## License
+
+	Copyright (c) 2015 Jin-Sih Lin
+	
+	MIT License
+	
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+	
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
 ## Contributing
 
 1. Fork it ( https://github.com/[my-github-username]/mothra/fork )
@@ -33,3 +60,4 @@ TODO: Write usage instructions here
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create a new Pull Request
+
--- lib/mothra/cli.rb
+++ lib/mothra/cli.rb
@@ -35,12 +35,12 @@ class CLI < Thor
     @bz = Rodzilla::Resource::Bug.new(@config['BZ_URL'], @config['BZ_USER'], @config['BZ_PASSWD'], :json)
   end
 
-  desc 'search <keyword>', 'search keyword from bugzilla summary for last 180 days'
-  def search(keyword)
-    half_years_ago = DateTime.now - 180
+  desc 'search <keyword>, <days_ago=180>', 'search keyword from bugzilla summary for last 180 days or you want'
+  def search(keyword, days_ago=180)
+    search_days = DateTime.now - days_ago.to_i
     results = @bz.search(product: @config['PRODUCT'], 
                          component: @config['COMPONENT'], 
-                         creation_time: half_years_ago, 
+                         creation_time: search_days, 
                          status: @config['STATUS'], 
                          summary: keyword
                         )
@@ -50,7 +50,7 @@ class CLI < Thor
         print "[#{r['id']}] ".yellow
         print "[#{r['status']}] ".cyan
         print "#{r['summary']} ".white
-        puts "[#{r['last_change_time']}] ".gray
+        puts "[#{r['last_change_time']}] ".light_black
       end
     else
       puts 'Not found.'
@@ -59,6 +59,58 @@ class CLI < Thor
 
   desc 'create <summary>', 'set PR summary and then `mothra add_file <attachment>`'
   def create(summary)
+    bug_id = _create(summary)
+
+    if not bug_id
+      puts 'Send-pr fail, please try again!'.yellow
+    else
+      print 'PR'
+      print " [#{bug_id}] ".yellow
+      puts "created!"
+      puts "#{@config['BZ_URL']}/show_bug.cgi?id=#{bug_id}".cyan
+    end
+  end
+
+  desc 'attach <bug_id>, <file_path>', 'add attachment to <bug_id>'
+  def attach(bug_id, file_path)
+    attach_id = _attach(bug_id, file_path)
+
+    if not attach_id
+      puts 'Fail to add attachments, please try again!'.yellow
+    else
+      print 'PR'
+      print " [#{bug_id}] ".cyan
+      print 'had add an attachment, '
+      puts " [#{attach_id}] ".yellow
+      puts "#{@config['BZ_URL']}/show_bug.cgi?id=#{bug_id}".cyan
+    end
+  end
+
+  desc 'submit <summary>, <file_path>', 'send-pr to bugs.freebsd.org'
+  def submit(summary, file_path)
+    bug_id = _create(summary)
+
+    if not bug_id
+      puts 'Send-pr fail, please try again!'.yellow
+      exit
+    end
+
+    attach_id = _attach(bug_id, file_path)
+
+    if not attach_id
+      puts 'Fail to add attachments, please upload attachment manually!'.yellow
+    else
+      print 'PR'
+      print " [#{bug_id}] ".cyan
+      print 'had add an attachment, '
+      puts " [#{attach_id}] ".yellow
+    end
+    puts "#{@config['BZ_URL']}/show_bug.cgi?id=#{bug_id}".cyan
+  end
+
+  private
+
+  def _create(summary)
     begin
       ret = @bz.create!(product: @config['PRODUCT'],
                         component: @config['COMPONENT'],
@@ -72,17 +124,13 @@ class CLI < Thor
                         summary: summary
                        )
 
-      print 'PR'
-      print " [#{ret['id']}] ".yellow
-      puts "created!"
-      puts "#{@config['BZ_URL']}/show_bug.cgi?id=#{ret['id']}".cyan
+      ret['id']
     rescue
-      puts 'Send-pr fail, please try again!'.yellow
+      false
     end
   end
 
-  desc 'attach <bug_id>, <file_path>', 'add attachment to <bug_id>'
-  def attach(bug_id, file_path)
+  def _attach(bug_id, file_path)
     file_name = get_file_name(file_path)
     file_base64 = base64(file_path)
 
@@ -93,16 +141,13 @@ class CLI < Thor
                                 summary: 'Just add an attachment.',
                                 content_type: 'text/plain',
                                )
-      print 'PR'
-      print " [#{bug_id}] ".cyan
-      print 'had add an attachment, '
-      puts " [#{ret['ids'][0]}] ".yellow
-      puts "#{@config['BZ_URL']}/show_bug.cgi?id=#{bug_id}".cyan
-    rescue
-      puts 'Fail to add attachments, please try again!'.yellow
-    end
 
+      ret['ids'][0]
+    rescue
+      false
+    end
   end
+  
 end
 
 CLI.start(ARGV)
